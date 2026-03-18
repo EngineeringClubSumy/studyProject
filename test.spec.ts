4aee4a51226e4452609e046f00f386f502ae28df
DeadShank <rigenkih1996@gmail.com>
2026-03-18 16:50:07 +0200

    test v3

--- test.spec.ts
+++ test.spec.ts
@@ -1,1 +1,7 @@
 import { test, expect } from '@playwright/test';
+
+test('Verify user can open cart page from header cart icon', async ({page}) => {
+    await page.goto('/');
+    await page.locator('a[class=cart-contents]').click();
+    await expect(page).toHaveURL(/cart/);
+});
