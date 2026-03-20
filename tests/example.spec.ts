f10bea16ef6fbef61962ca915338ce9fe01b03a8
jkep-ved <v8288287@gmail.com>
2026-03-20 17:07:04 +0200

    Remove default Playwright example test

--- tests/example.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('home page has Playwright title', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle(/Playwright/);
-});
